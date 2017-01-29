405c790b7f2d226b3a21cb36361754d76f2456c1
Daniel <wilkowski.kontakt@gmail.com>
2017-01-29 15:00:10 +0100

    Clean

--- src/com/sad/sadcrm/MailUtil.java
+++ src/com/sad/sadcrm/MailUtil.java
@@ -3,34 +3,22 @@ package com.sad.sadcrm;
 import com.sad.sadcrm.model.ClientConstants;
 
 import javax.mail.*;
+import javax.mail.Message.RecipientType;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class MailUtil {
-    private final static String username = "sadcrm@gmail.com";
-    private final static String password = "aplikacja";
+    private final static String USERNAME = "sadcrm@gmail.com";
+    private final static String PASSWORD = "aplikacja";
 
     public static String sendMail(String recipients, String subject, String content) {
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props,
-                new Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
+        Session session = Session.getInstance(getProperties(), getAuthenticator());
 
         try {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("sadcrm@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
-
+            message.setRecipients(RecipientType.TO, InternetAddress.parse(recipients));
             message.setSubject(subject);
             message.setText(content);
 
@@ -40,5 +28,22 @@ public class MailUtil {
         } catch (MessagingException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        return properties;
+    }
+
+    private static Authenticator getAuthenticator() {
+        return new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, PASSWORD);
+            }
+        };
     }
 }
--- src/com/sad/sadcrm/TableUtil.java
+++ src/com/sad/sadcrm/TableUtil.java
@@ -12,123 +12,115 @@ public class TableUtil {
         tableClients.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         tableClients.setFillsViewportHeight(true);
 
-        DefaultTableModel uneditableModel = createUneditableTableModel();
+        DefaultTableModel model = getUneditableTableModel();
 
-        uneditableModel.setColumnIdentifiers(getColumnNames());
+        Object[] tableColumnNames = new Object[6];
+        tableColumnNames[0] = "Id";
+        tableColumnNames[1] = "Name";
+        tableColumnNames[2] = "Nazwisko";
+        tableColumnNames[3] = "Pesel";
+        tableColumnNames[4] = "Mail";
+        tableColumnNames[5] = "Opiekun";
+
+        model.setColumnIdentifiers(tableColumnNames);
         if (clients == null) {
-            tableClients.setModel(uneditableModel);
+            tableClients.setModel(model);
             return;
         }
 
         for (Client client : clients) {
-            uneditableModel.addRow(getTableModelForClient(client));
+            model.addRow(getColumnsForClient(client));
         }
 
-        tableClients.setModel(uneditableModel);
-    }
-
-    private static DefaultTableModel createUneditableTableModel() {
-        return new DefaultTableModel() {
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                return false;
-            }
-        };
-    }
-
-    private static Object[] getTableModelForClient(Client client) {
-        Object[] objects = new Object[6];
-        objects[0] = client.getIdClient();
-        objects[1] = client.getName();
-        objects[2] = client.getSurname();
-        objects[3] = client.getPesel();
-        objects[4] = client.getMail();
-        objects[5] = client.getUser().getName() + " " + client.getUser().getSurname();
-        return objects;
-    }
-
-    private static Object[] getColumnNames() {
-        Object[] columnNames = new Object[6];
-        columnNames[0] = "Id";
-        columnNames[1] = "Name";
-        columnNames[2] = "Nazwisko";
-        columnNames[3] = "Pesel";
-        columnNames[4] = "Mail";
-        columnNames[5] = "Opiekun";
-        return columnNames;
+        tableClients.setModel(model);
     }
 
     public static void displayUsers(List<User> users, JTable tableUsers) {
         tableUsers.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         tableUsers.setFillsViewportHeight(true);
 
-        DefaultTableModel uneditableModel = createUneditableTableModel();
+        DefaultTableModel model = getUneditableTableModel();
 
-        uneditableModel.setColumnIdentifiers(getUserColumnNames());
-        if (users == null) {
-            tableUsers.setModel(uneditableModel);
-            return;
-        }
-
-        for (User user : users) {
-            uneditableModel.addRow(getTableForUser(user));
-        }
-
-        tableUsers.setModel(uneditableModel);
-    }
-
-    private static Object[] getUserColumnNames() {
         Object[] tableColumnNames = new Object[5];
         tableColumnNames[0] = "Id";
         tableColumnNames[1] = "Name";
         tableColumnNames[2] = "Nazwisko";
         tableColumnNames[3] = "Login";
         tableColumnNames[4] = "Data utworzenia";
-        return tableColumnNames;
-    }
 
-    private static Object[] getTableForUser(User user) {
-        Object[] objects = new Object[5];
-        objects[0] = user.getId();
-        objects[1] = user.getName();
-        objects[2] = user.getSurname();
-        objects[3] = user.getLogin();
-        objects[4] = user.getCreated();
-        return objects;
+        model.setColumnIdentifiers(tableColumnNames);
+        if (users == null) {
+            tableUsers.setModel(model);
+            return;
+        }
+
+        for (User user : users) {
+
+            model.addRow(getColumnsForUser(user));
+        }
+
+        tableUsers.setModel(model);
     }
 
     public static void displayUsersForManager(List<User> users, JTable tableUsers) {
         tableUsers.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         tableUsers.setFillsViewportHeight(true);
 
-        DefaultTableModel uneditableTableModel = createUneditableTableModel();
+        DefaultTableModel model = getUneditableTableModel();
 
-        uneditableTableModel.setColumnIdentifiers(createColumnsForUsers());
-        if (users == null) {
-            tableUsers.setModel(uneditableTableModel);
-            return;
-        }
-
-        for (User user : users) {
-            uneditableTableModel.addRow(createColumnsForUser(user));
-        }
-
-        tableUsers.setModel(uneditableTableModel);
-    }
-
-    private static Object[] createColumnsForUser(User user) {
-        Object[] objects = new Object[3];
-        objects[0] = user.getId();
-        objects[1] = user.getName();
-        objects[2] = user.getSurname();
-        return objects;
-    }
-
-    private static Object[] createColumnsForUsers() {
         Object[] tableColumnNames = new Object[3];
         tableColumnNames[0] = "Id";
         tableColumnNames[1] = "Name";
         tableColumnNames[2] = "Nazwisko";
-        return tableColumnNames;
+
+        model.setColumnIdentifiers(tableColumnNames);
+        if (users == null) {
+            tableUsers.setModel(model);
+            return;
+        }
+
+        for (User user : users) {
+            model.addRow(getColumnsForUserForManager(user));
+        }
+
+        tableUsers.setModel(model);
+    }
+
+    private static Object[] getColumnsForClient(Client client) {
+        return new Object[]{
+                client.getIdClient(),
+                client.getName(),
+                client.getSurname(),
+                client.getPesel(),
+                client.getMail(),
+                client.getUser().getName() + " " + client.getUser().getSurname()
+        };
+    }
+
+    private static Object[] getColumnsForUser(User user) {
+        return new Object[]{
+                user.getId(),
+                user.getName(),
+                user.getSurname(),
+                user.getLogin(),
+                user.getCreated()
+        };
+    }
+
+    private static Object[] getColumnsForUserForManager(User user) {
+        return new Object[]{
+                user.getId(),
+                user.getName(),
+                user.getSurname()
+        };
+    }
+
+    private static DefaultTableModel getUneditableTableModel() {
+        return new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
     }
 }
