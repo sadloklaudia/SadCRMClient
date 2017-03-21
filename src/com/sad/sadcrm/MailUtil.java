17aa042a5709e992d853ab0d6460641d93e3903e
Daniel <wilkowski.kontakt@gmail.com>
2017-03-21 12:20:55 +0100

    Clean

--- src/com/sad/sadcrm/MailUtil.java
+++ src/com/sad/sadcrm/MailUtil.java
@@ -3,12 +3,12 @@ package com.sad.sadcrm;
 import static com.sad.sadcrm.Parameters.getCredentials;
 
 public class MailUtil {
-    public static void sendMail(String recipients, String subject, String content) {
+    public static void sendMail(String recipients, String subject, String message) {
         try {
             HttpJson.post("/mail", getCredentials()
                     .add("recipients", recipients)
                     .add("subject", subject)
-                    .add("message", content));
+                    .add("message", message));
         } catch (HttpJsonException exception) {
             throw new SendMailException(exception.getMessage());
         }
