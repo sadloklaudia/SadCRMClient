ca1f42027f50bdffb1c9f2d63a32ca64818bdff5
Daniel <wilkowski.kontakt@gmail.com>
2017-03-21 11:56:27 +0100

    Fixed sending mail

--- src/com/sad/sadcrm/MailUtil.java
+++ src/com/sad/sadcrm/MailUtil.java
@@ -5,7 +5,10 @@ import static com.sad.sadcrm.Parameters.getCredentials;
 public class MailUtil {
     public static void sendMail(String recipients, String subject, String content) {
         try {
-            HttpJson.post("/mail", getCredentials());
+            HttpJson.post("/mail", getCredentials()
+                    .add("recipients", recipients)
+                    .add("subject", subject)
+                    .add("message", content));
         } catch (HttpJsonException exception) {
             throw new SendMailException(exception.getMessage());
         }
