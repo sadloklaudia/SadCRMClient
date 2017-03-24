7bcf0fe9c40a6623a8dcdb020c685cd8a2146d4a
Daniel <wilkowski.kontakt@gmail.com>
2017-03-24 21:43:31 +0100

    Rewrited User to HTTP (from hibernate)

--- src/com/sad/sadcrm/hibernate/UserDAO.java
+++ src/com/sad/sadcrm/hibernate/UserDAO.java
@@ -1,27 +1,29 @@
 package com.sad.sadcrm.hibernate;
 
+import com.sad.sadcrm.HttpJson;
 import com.sad.sadcrm.HttpJsonException;
 import com.sad.sadcrm.Parameters;
 import com.sad.sadcrm.UserLoginException;
 import com.sad.sadcrm.hibernate.exception.UserInsertException;
 import com.sad.sadcrm.hibernate.exception.UserUpdateException;
 import com.sad.sadcrm.model.User;
-import org.hibernate.Query;
-import org.hibernate.Session;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.sad.sadcrm.HttpJson.post;
 import static com.sad.sadcrm.Parameters.getCredentials;
+import static com.sad.sadcrm.model.User.createFromJson;
 
 public class UserDAO {
     public static User login(String login, String password) {
         try {
             Parameters.useCredentials(login, password);
             JSONObject jsonUser = post("/user/login", getCredentials());
-            return User.createFromJson(jsonUser.getJSONObject("user"));
+            return createFromJson(jsonUser.getJSONObject("user"));
         } catch (JSONException exception) {
             throw new RuntimeException("Missing \"id\" key", exception);
         } catch (HttpJsonException exception) {
@@ -45,44 +47,55 @@ public class UserDAO {
         }
     }
 
-    public static List<User> searchUsersBySurnameAndName(String surname, String name) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User where surname = :surname and name = :name");
-        query.setParameter("surname", surname);
-        query.setParameter("name", name);
-
-        return query.list();
-    }
-
-    public static List<User> searchUsersBySurname(String surname) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User where surname = :surname");
-        query.setParameter("surname", surname);
-
-        return query.list();
-    }
-
-    public static List<User> searchUsersByName(String name) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User where name = :name");
-        query.setParameter("name", name);
-
-        return query.list();
+    public static User getUserById(Integer id) {
+        return fetchUserById(getCredentials()
+                .add("id", Integer.toString(id)));
     }
 
     public static List<User> findUsers() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User");
-
-        return query.list();
+        return fetchUsersByParameters(getCredentials());
     }
 
-    public static User getUserById(Integer id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User where id=:id");
-        query.setParameter("id", id);
-
-        return (User) query.list().get(0);
+    public static List<User> searchUsersBySurnameAndName(String surname, String name) {
+        return fetchUsersByParameters(getCredentials()
+                .add("surname", surname)
+                .add("name", name));
     }
 
+    public static List<User> searchUsersBySurname(String surname) {
+        return fetchUsersByParameters(getCredentials()
+                .add("surname", surname));
+    }
+
+    public static List<User> searchUsersByName(String name) {
+        return fetchUsersByParameters(getCredentials()
+                .add("name", name));
+    }
+
+    private static User fetchUserById(Parameters parameters) {
+        try {
+            JSONObject object = HttpJson.get("/user", parameters);
+            return User.createFromJson(object);
+        } catch (HttpJsonException e) {
+            throw new RuntimeException("Could not fetch user", e);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<User> fetchUsersByParameters(Parameters parameters) {
+        try {
+            JSONObject object = HttpJson.get("/user", parameters);
+            List<User> users = new ArrayList<>();
+            JSONArray jsonUsers = object.getJSONArray("users");
+            for (int i = 0; i < jsonUsers.length(); i++) {
+                users.add(createFromJson(jsonUsers.getJSONObject(i)));
+            }
+            return users;
+        } catch (HttpJsonException e) {
+            throw new RuntimeException("Could not fetch user", e);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
