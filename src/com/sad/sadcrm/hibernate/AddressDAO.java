4a31447d777c56bf9725830214b12d7d9de5d646
Klaudia <sadloklaudia@gmai.com>
2017-03-24 19:23:10 +0100

    Fix adding addresses

--- src/com/sad/sadcrm/hibernate/AddressDAO.java
+++ src/com/sad/sadcrm/hibernate/AddressDAO.java
@@ -1,12 +1,24 @@
 package com.sad.sadcrm.hibernate;
 
+import com.sad.sadcrm.HttpJsonException;
 import com.sad.sadcrm.model.Address;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import static com.sad.sadcrm.HttpJson.post;
 import static com.sad.sadcrm.HttpJson.postHTML;
+
 
 public class AddressDAO {
     public static void insert(Address address) {
-        postHTML("/address/create", address.asParameters());
+        try {
+            JSONObject result = post("/address/create", address.asParameters());
+            address.setId(result.getInt("id"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (HttpJsonException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void update(Address address) {
