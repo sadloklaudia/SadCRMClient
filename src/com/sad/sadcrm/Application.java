c79297fd3dbc5e299dbbd979912976dd9fabad7a
Daniel <wilkowski.kontakt@gmail.com>
2017-04-11 13:30:20 +0200

    Clean

--- src/com/sad/sadcrm/Application.java
+++ src/com/sad/sadcrm/Application.java
@@ -2,15 +2,9 @@ package com.sad.sadcrm;
 
 import com.sad.sadcrm.form.SadCRMForm;
 
-import javax.swing.UIManager.LookAndFeelInfo;
-import javax.swing.*;
-
+import static com.sad.sadcrm.LookAndFellUtil.setLookAndFeelIfExists;
 import static java.awt.EventQueue.invokeLater;
 import static java.lang.Thread.setDefaultUncaughtExceptionHandler;
-import static java.util.logging.Level.SEVERE;
-import static java.util.logging.Logger.getLogger;
-import static javax.swing.UIManager.getInstalledLookAndFeels;
-import static javax.swing.UIManager.setLookAndFeel;
 
 public class Application {
     public static final String VERSION = "0.8.1";
@@ -19,17 +13,5 @@ public class Application {
         setLookAndFeelIfExists("Windows");
         setDefaultUncaughtExceptionHandler(new DefaultExceptionHandler());
         invokeLater(() -> new SadCRMForm().setVisible(true));
-    }
-
-    private static void setLookAndFeelIfExists(String lookAndFeelName) {
-        try {
-            for (LookAndFeelInfo info : getInstalledLookAndFeels()) {
-                if (lookAndFeelName.equals(info.getName())) {
-                    setLookAndFeel(info.getClassName());
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException ex) {
-            getLogger(SadCRMForm.class.getName()).log(SEVERE, null, ex);
-        }
     }
 }
---
+++ src/com/sad/sadcrm/LookAndFellUtil.java
@@ -0,0 +1,24 @@
+package com.sad.sadcrm;
+
+import com.sad.sadcrm.form.SadCRMForm;
+
+import javax.swing.*;
+
+import static java.util.logging.Level.SEVERE;
+import static java.util.logging.Logger.getLogger;
+import static javax.swing.UIManager.getInstalledLookAndFeels;
+import static javax.swing.UIManager.setLookAndFeel;
+
+class LookAndFellUtil {
+    static void setLookAndFeelIfExists(String lookAndFeelName) {
+        try {
+            for (UIManager.LookAndFeelInfo info : getInstalledLookAndFeels()) {
+                if (lookAndFeelName.equals(info.getName())) {
+                    setLookAndFeel(info.getClassName());
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException ex) {
+            getLogger(SadCRMForm.class.getName()).log(SEVERE, null, ex);
+        }
+    }
+}
